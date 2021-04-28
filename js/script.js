2d311786405fe6e3d223a8f2e3047042fee9945d
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-28 16:55:05 +0200

    Stampato le option con v-for

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            music: []
+            music: [],
+            genres : [],
+            selectAll: 'All'
         },
         methods: {
+            /*
             filterArray(array, val) {
                 let filteredArray = [];
                 if( val == "all" ) {
@@ -17,15 +20,35 @@ var app = new Vue(
                     
                 return filteredArray;
             }
+            */
         },
         mounted() {
             axios
                 .get('https://flynn.boolean.careers/exercises/api/array/music')
                 .then((response) => {
+
                     this.music = response.data.response;
+
                     this.music.sort((a, b) => {
                         return a.year - b.year;
-                    })
+                    });
+
+                    console.log(this.music);
+
+                    this.music.forEach((item, index) => {
+                        if( !this.genres.includes(item.genre) ) {
+                            this.genres.push(item.genre);
+                        }
+                        
+                    });
+
+                    console.log(this.genres);
+
+                    // problema 1 : sono duplicato
+                    // poi : come faccio a stampare in html dentro la select i valori di questi generi?
+
+                    // come faccio a sapere qua è il genere selezionato? v-model
+
                 })
         }
     }
