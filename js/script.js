533505203fe58a07a22b12b3407c88b406e46ab2
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-28 14:50:02 +0200

    Fatta prima parte esercizio

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            
+            music: []
         },
         methods: {
 
         },
         mounted() {
-            
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then((response) => {
+                    this.music = response.data.response;
+                })
         }
     }
 )
